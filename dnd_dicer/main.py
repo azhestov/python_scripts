3d2e995cdecc72d5d20353ae6b660006eb73ba3e
Anatoliy Zhestov <azhestov@gmail.com>
2022-06-12 04:40:07 +0400

    add dnd dicer

---
+++ dnd_dicer/main.py
@@ -0,0 +1,40 @@
+import random
+
+doing = (2, 4, 6, 8, 10, 12, 20)
+done = False
+import os
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+# now, to clear the screen
+
+while not done:
+    print('DND dicer')
+    print('Choose your dice in xdx format 4d6 for example')
+
+    what = 0
+    amount = 0
+    while what not in doing:
+        dices = input("Dices: ")
+        if not dices.find('d'):
+            print('something wrong')
+            continue
+        arr = dices.split('d')
+        if len(arr) != 2:
+            print('something wrong')
+            continue
+        try:
+            what = abs(int(arr[1]))
+            amount = abs(int(arr[0]))
+        except ValueError:
+            print('something wrong')
+
+    fins = []
+    for i in range(amount):
+        fins.append(random.randint(1, int(what)))
+        print(fins[i], end=' | ')
+    print("Sum = ", sum(fins))
+    if input('Press \'y\' for repeat: ') != 'y':
+        done = True
+    cls()
